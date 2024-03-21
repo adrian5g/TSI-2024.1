4ab2719f2025bb8c0e85d09b3e7cacf27f0bf03b
Onyzn <jogadorsquarebr@gmail.com>
2024-03-21 11:43:56 -0300

    Aula Anibal 21/03

--- fibonacci/Fibonacci.c
+++ fibonacci/Fibonacci.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
 int main() {
+    int n;
+
+    printf("Digite o numero que deseja\n");
+    scanf("%i", &n);
+
     int numero_anterior = 0;
     int numero_atual = 1;
 
     int i;
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < n; i++) {
         int num = numero_atual;
 
         numero_atual = num + numero_anterior;
         numero_anterior = num;
-
-        printf("%i", numero_atual);
     }
+
+    printf("o numero %i da sequencia de fibonacci: %i", n, numero_atual);
 }
