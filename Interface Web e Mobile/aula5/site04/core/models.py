8db8aab804bc3869d6fc2ce4e3caf885cc7a0034
adrian5g <jogadorsquarebr@gmail.com>
2024-12-13 00:46:37 -0300

    feito CRUD no site04

--- Interface Web e Mobile/aula5/site04/core/models.py
+++ Interface Web e Mobile/aula5/site04/core/models.py
@@ -9,16 +9,20 @@ from django.db import models
 # CharField      - Texto com Limite
 # TextField      - Texto sem Limite
 
+
 class Area(models.Model):
-  nome = models.CharField('Nome', max_length=100)
+    nome = models.CharField('Nome', max_length=100)
+
 
 class Publico(models.Model):
-  nome = models.CharField('Nome', max_length=100)
+    nome = models.CharField('Nome', max_length=100)
+
 
 class Curso(models.Model):
-  titulo = models.CharField('Título', max_length=200)
-  descricao = models.TextField('Descrição')
-  autor = models.CharField('Autor', max_length=100)
-  vagas = models.IntegerField('Vagas')
-  area = models.ForeignKey(Area, on_delete=models.PROTECT) # PROTECT -> Protege | CASCADE -> Apaga as dependências
-  publicos = models.ManyToManyField(Publico)
+    titulo = models.CharField('Título', max_length=200)
+    descricao = models.TextField('Descrição')
+    autor = models.CharField('Autor', max_length=100)
+    vagas = models.IntegerField('Vagas')
+    # PROTECT -> Protege | CASCADE -> Apaga as dependências
+    area = models.ForeignKey(Area, on_delete=models.PROTECT)
+    publicos = models.ManyToManyField(Publico)
